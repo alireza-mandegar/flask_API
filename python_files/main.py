8ff169d89470c6c9cb4879b9520d21fcaa149017
alireza-mandegar <alireza.mani95@gmail.com>
2021-12-03 20:15:32 +0330

    db has been created

--- python_files/main.py
+++ python_files/main.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
-class model(db.Model):
+class Model(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     slug = db.Column(db.String, nullable=False)
@@ -19,8 +19,11 @@ class model(db.Model):
     o = db.Column(db.Integer, nullable=False)
     h = db.Column(db.Integer, nullable=False)
     l = db.Column(db.Integer, nullable=False)
-    t = db.column(db.String, nullable=False)
+    t = db.Column(db.String, nullable=False)
     update_at = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return f"model(id:{self.id}, title:{self.title}, slug:{self.slug}, p:{self.p}, d:{self.d}, dp:{self.dp}, dt:{self.dt}, o:{self.o}, h:{self.h}, l:{self.l}, t:{self.t}, updated_at:{self.update_at})"
+
+# db.create_all() db has been created don't run this line
+
