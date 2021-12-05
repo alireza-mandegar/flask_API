3fc9c455deeb1518eeb612303a1f5e96dad43d74
alireza-mandegar <alireza.mani95@gmail.com>
2021-12-05 12:41:20 +0330

    model_put_args added

--- python_files/main.py
+++ python_files/main.py
@@ -25,15 +25,30 @@ class Model(db.Model):
     def __repr__(self):
         return f"model(id:{self.id}, title:{self.title}, slug:{self.slug}, p:{self.p}, d:{self.d}, dp:{self.dp}, dt:{self.dt}, o:{self.o}, h:{self.h}, l:{self.l}, t:{self.t}, updated_at:{self.update_at})"
 
+
 # db.create_all() db has been created don't run this line
 
+model_put_args = reqparse.RequestParser()
+model_put_args.add_argument("id", type=int, help="id of model", required=True)
+model_put_args.add_argument("title", type=str, help="title of model", required=True)
+model_put_args.add_argument("slug", type=str, help="slug of model", required=True)
+model_put_args.add_argument("p", type=int, help="p of model", required=True)
+model_put_args.add_argument("d", type=int, help="d of model", required=True)
+model_put_args.add_argument("dp", type=int, help="dp of model", required=True)
+model_put_args.add_argument("dt", type=str, help="dt of model", required=True)
+model_put_args.add_argument("o", type=int, help="o of model", required=True)
+model_put_args.add_argument("h", type=int, help="h of model", required=True)
+model_put_args.add_argument("l", type=int, help="l of model", required=True)
+model_put_args.add_argument("t", type=str, help="t of model", required=True)
+model_put_args.add_argument("update_at", type=str, help="update_at of model", required=True)
+
 resource_fields = {
-	'id': fields.Integer,
-	'title': fields.String,
-	'slug': fields.String,
-	'p': fields.Integer,
-	'd': fields.Integer,
-	'dp': fields.Integer,
+    'id': fields.Integer,
+    'title': fields.String,
+    'slug': fields.String,
+    'p': fields.Integer,
+    'd': fields.Integer,
+    'dp': fields.Integer,
     'dt': fields.String,
     'o': fields.Integer,
     'h': fields.Integer,
